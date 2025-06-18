8e5b34a34b7694591348e42494aa5feb3ec78ef3
@leandro <leandro@lerpsolutions.com>
2025-06-18 08:43:45 -0300

    Update filesystems.php

--- config/filesystems.php
+++ config/filesystems.php
@@ -47,7 +47,7 @@ return [
         'products' => [
             'driver' => 'local',
             'root' => storage_path('app/public/products'),
-            'url' => env('APP_URL').'/storage/products',
+            'url' => env('APP_URL').'storage/products',
             'visibility' => 'public',
             'throw' => false,
         ],
